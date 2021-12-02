c2540e364c0f58ece094819eeb1d6311462add9b
SaidYein <saidyein@gmail.com>
2021-12-02 16:47:20 +0100

    basic css + fetching data

---
+++ src/helpers.js
@@ -0,0 +1,38 @@
+// Formats response to look presentable on webpage
+const renderCountry = function (data) {
+  console.log(data)
+  const html = `
+  <article>
+    <img class="country__img" src="${data[0].flags.png}" />
+    <div class="country__data">
+      <h3 class="country__name">${data[0].name.common}</h3>
+      <h4 class="country__region">${data[0].region}</h4>
+      <p class="country__row"><span>👫</span>${(
+        +data[0].population / 1000000
+      ).toFixed(1)} people</p>
+      <p class="country__row"><span>🗣️</span>${data[0].capital[0]}</p>
+      <p class="country__row"><span>💰</span>${data[0].currencies}</p>
+    </div>
+  </article>
+  `;
+  countriesContainer.insertAdjacentHTML('beforeend', html);
+  countriesContainer.style.opacity = 1;
+};
+
+
+ const displayCountry = function(event) {
+  event.preventDefault();
+  while (countriesContainer.firstChild) {
+    countriesContainer.removeChild(countriesContainer.firstChild);
+  }
+  return getCountry(countryInput.value);
+};
+
+const displayCountries = function(event) {
+  event.preventDefault();
+  while (countriesContainer.firstChild) {
+    countriesContainer.removeChild(countriesContainer.firstChild);
+  }
+  return getCountryAndNeighbors(countryInput.value);
+};
+
---
+++ src/script.js
@@ -0,0 +1,36 @@
+// Selecting page elements
+const btn = document.querySelector('.btn-country');
+const countriesContainer = document.querySelector('.countries');
+const countryInput = document.querySelector('.form-control')
+
+const getCountry = async (country) => {
+  const response = await fetch('https://restcountries.com/v3.1/name/'+country)
+  if(response.ok){
+    return response.json()
+  }else{
+    console.log('fetch failed')
+  }
+}
+
+// const getBackground = async () => {
+//   const query = `https://api.unsplash.com/photos/random`
+//   const response  = await fetch(query)
+//   if(response.ok){
+//     console.log(response)
+//   }else {
+//     console.log('background failed')
+//   }
+// }
+
+const main = async ()=>{
+  try{
+    await getBackground()
+    const country = await getCountry(countryInput.value);
+    renderCountry(country)
+  }catch(error) {
+    console.log(error)
+  }
+}
+
+
+btn.addEventListener('click', main)
