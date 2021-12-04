b5a22b76419aeebf937b6c883cde2d579cd5d72f
SaidYein <saidyein@gmail.com>
2021-12-04 17:29:17 +0100

    clearing previous html on new search

--- src/script.js
+++ src/script.js
@@ -17,6 +17,26 @@ const renderError = (error) => {
   console.log(error)
 }
 
+// Formats response to look presentable on webpage
+const renderCountry = function (data, className = '') {
+  
+  const html = `
+  <article class= 'country ${className}'>
+    <img class="country__img" src="${data[0].flags.png}" />
+    <div class="country__data">
+      <h3 class="country__name">${data[0].name.common}</h3>
+      <h4 class="country__region">${data[0].region}</h4>
+      <p class="country__row"><span>👫</span>${(
+        +data[0].population / 1000000
+      ).toFixed(1)} people</p>
+      <p class="country__row"><span>🏛</span>${data[0].capital[0]}</p>
+      <p class="country__row"><span>💰</span>${Object.keys(data[0].currencies)[0]}</p>
+    </div>
+  </article>
+  `;
+  countriesContainer.insertAdjacentHTML('beforeend', html);
+};
+
 // get country entered from UI
 const getCountry = async (country) => {
   try {
@@ -60,7 +80,10 @@ const renderNeighbors = (data)=>{
 //   }
 // }
 
+
+
 const main = async ()=>{
+  countriesContainer.innerHTML= '';
   try{
     //await getBackground()
     const country = await getCountry(countryInput.value);
