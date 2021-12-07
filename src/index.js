3c648dff52c6e0b4f8d3d30f66986871ab016708
SaidYein <saidyein@gmail.com>
2021-12-07 18:15:24 +0100

    ..

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 "use strict";
 
-import { btn } from "./src/constants.js";
-import { searchButtonListener } from "./src/handlers/handlers.js";
+import { btn } from "./constants.js";
+import { searchButtonListener } from "./handlers/handlers.js";
 
 // press enter to search
 const input = document.getElementById("myInput");
